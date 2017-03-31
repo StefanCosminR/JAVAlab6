2528e0a1ff4beafc8cb5df6a4a303d08c7a2fb75
StefanCosminR <stefancosmin.romanescu@gmail.com>
2017-03-31 13:33:46 +0300

    Finished Interpolation

--- src/Views/InterpolationPointsInput.java
+++ src/Views/InterpolationPointsInput.java
@@ -1,5 +1,7 @@
 package Views;
 
+import Listeners.BuildGraphicInterpolationButtonListener;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -9,13 +11,13 @@ import java.awt.FlowLayout;
 /**
  * Created by stefanromanescu on 31/03/17.
  */
-public class InterpolationPoints extends JPanel {
-    private static InterpolationPoints instance;
+public class InterpolationPointsInput extends JPanel {
+    private static InterpolationPointsInput instance;
     private JLabel coordinatesLabel;
     private JTextField coordinates;
     private JButton buildGraphicButton;
 
-    private InterpolationPoints() {
+    private InterpolationPointsInput() {
         coordinatesLabel = new JLabel("Points for interpolation :");
         coordinates = new JTextField(50);
         buildGraphicButton = new JButton("Build Graphic");
@@ -23,12 +25,17 @@ public class InterpolationPoints extends JPanel {
         this.setLayout(new FlowLayout());
         this.add(coordinatesLabel);
         this.add(coordinates);
+        buildGraphicButton.addActionListener(new BuildGraphicInterpolationButtonListener(this));
         this.add(buildGraphicButton);
     }
 
-    public static InterpolationPoints getInstance() {
+    public JTextField getCoordinates() {
+        return coordinates;
+    }
+
+    public static InterpolationPointsInput getInstance() {
         if(instance == null) {
-            instance = new InterpolationPoints();
+            instance = new InterpolationPointsInput();
         }
         return instance;
     }
